72732bbd46d17dc0b7aee60212610d34a21713ee
suneha11 <suneha.ss1502@gmail.com>
2024-05-07 02:20:37 +0530

    adding exports to zod file

--- todo-app/backend/types.js
+++ todo-app/backend/types.js
@@ -18,5 +18,10 @@ const createTodo = zod.object({
 })
 
 const updateTodo = zod.object({
-    id : zod.string();
+    id : zod.string(),
 })
+
+module.exports={
+    createTodo: createTodo,
+    updateTodo: updateTodo
+}
