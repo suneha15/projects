eb5c95c92237a409edc3c1012c30645c0d4a8330
suneha11 <suneha.ss1502@gmail.com>
2024-05-07 17:30:47 +0530

    hitting the BE and getting all todos

--- todo-app/frontend/src/App.jsx
+++ todo-app/frontend/src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ todos, setTodos] = useState([]);
+
+  fetch("http://localhost:3000/todos")
+    .then( async function(res){
+      const json = await res.json();
+      setTodos(json.todos);
+    })
 
   return (
     <>
     <div>
-      hi there
+      <CreateTodo></CreateTodo>
+      <Todos todos = {todos}></Todos>
     </div>
     </>
   )
