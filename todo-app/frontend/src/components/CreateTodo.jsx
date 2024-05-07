8abc745379b55b17ac7e75a637bbfac3e3682033
suneha11 <suneha.ss1502@gmail.com>
2024-05-07 15:30:07 +0530

    adding todo component

---
+++ todo-app/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+
+
+export function CreateTodo(){
+    return (
+    <div>
+        <input type="text" placeholder="title" id="" /><br />
+        <input type="text" placeholder="description" id="" /><br /><br />
+        <button>Add a todo</button>
+    </div>
+    )
+}
