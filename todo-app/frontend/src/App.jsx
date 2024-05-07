8c335544da899fad54d263b81f9a6102a90e8baf
suneha11 <suneha.ss1502@gmail.com>
2024-05-07 18:35:21 +0530

    adding a todo from frontend

--- todo-app/frontend/src/App.jsx
+++ todo-app/frontend/src/App.jsx
@@ -8,11 +8,12 @@ import { Todos } from './components/Todos'
 function App() {
   const [ todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos")
-    .then( async function(res){
-      const json = await res.json();
-      setTodos(json.todos);
-    })
+//useEffect hook
+  // fetch("http://localhost:3000/todos")
+  //   .then( async function(res){
+  //     const json = await res.json();
+  //     setTodos(json.todos);
+  //   })
 
   return (
     <>
