4ae1023c674dd1d6dae38e4e4a6178c8a36b0494
suneha11 <suneha.ss1502@gmail.com>
2024-08-15 00:40:06 +0530

    adding components

---
+++ Expense-tracker/frontend/src/components/TransactionList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const TransactionList = () => {
+  return (
+    <div>
+        <h3>History</h3>
+        <ul id='list' className='list'>
+            <li>camera</li>
+            <li>eggs</li>
+            <li>shopping</li>
+        </ul>
+    </div>
+  )
+}
