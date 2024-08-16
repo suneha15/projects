d070e25133c1ff961f938dc6bcd05312f922df0f
suneha11 <suneha.ss1502@gmail.com>
2024-08-16 20:39:16 +0530

    basic context-api

---
+++ Context-api/minicontext/src/context/UserContext.js
@@ -0,0 +1,10 @@
+import React,{createContext} from 'react';
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+{/* <userContext>
+    <Login/>
+    <Card/>
+<userContext/> */}
